f3adf3d94714758e5315b580bd301af6ff0319be
Othman El Hammouchi <othman.el.hammouchi@hotmail.com>
2024-04-14 11:54:44 +0200

    Add the Fortran simulation code for the ODP model.
    Operations on the claims triangle which require context data are encapsulated inside a ligthweight Fortran derived type.

---
+++ src/mod_odp.f90
@@ -0,0 +1,220 @@
+module mod_odp
+
+  use, intrinsic :: iso_c_binding
+  use, intrinsic :: omp_lib
+  use, intrinsic :: ieee_arithmetic
+  use mod_config
+  use mod_random
+  use mod_pgb
+  use mod_odp_triangle
+
+  implicit none
+
+contains
+
+  function odp_param_boot(trngl, n_boot, n_sim, dist, pgb, status) result(reserve)
+    type(t_odp_triangle), intent(inout) :: trngl
+    integer(c_int), intent(in) :: n_boot, n_sim, dist
+    type(c_ptr), intent(in), value :: pgb
+    integer, intent(inout) :: status
+
+    real(c_double) :: reserve(n_boot * n_sim)
+    integer(c_int) :: n_pts
+    real(c_double) :: X_pred(trngl%n_pred, trngl%n_cov), y_pred(trngl%n_pred)
+    real(c_double) :: triangle_boot(trngl%n_dev, trngl%n_dev), betas_boot(trngl%n_cov)
+    real(c_double) :: triangle_pred(trngl%n_dev, trngl%n_dev)
+
+    real(c_double) :: lambda, mean, sd, shape, scale
+    real(c_double), allocatable :: flat_resids(:)
+    integer(c_int) :: i, j, k, i_boot, i_sim
+    logical :: progress
+
+    progress = c_associated(pgb)
+
+    n_pts = count(trngl%mask)
+    allocate (flat_resids(n_pts), source=0._c_double)
+    call trngl%fit_glm(use_mask=.true., status=status)
+
+    i_boot = 1
+    main_loop: do while (i_boot <= n_boot)
+
+      triangle_boot = 0
+      do i = 1, trngl%n_dev
+        do j = 1, trngl%n_dev + 1 - i
+          if (dist == NORMAL) then
+            mean = trngl%triangle_fit(i, j)
+            sd = sqrt(trngl%disp * trngl%triangle_fit(i, j))
+            triangle_boot(i, j) = rnorm(mean, sd)
+            if (triangle_boot(i, j) < 0) cycle main_loop
+
+          else if (dist == GAMMA) then
+            shape = trngl%triangle_fit(i, j)**2 / (trngl%disp * trngl%triangle_fit(i, j))
+            scale = (trngl%disp * trngl%triangle_fit(i, j)) / trngl%triangle_fit(i, j)
+            triangle_boot(i, j) = rgamma(shape, scale)
+
+          else if (dist == POISSON) then
+            lambda = trngl%triangle_fit(i, j) / trngl%disp
+            triangle_boot(i, j) = trngl%disp * rpois(lambda)
+          end if
+        end do
+      end do
+
+      call trngl%fit_glm(use_mask=.false., status=status)
+      if (status == FAILURE) cycle main_loop
+
+      X_pred = 0.0
+      X_pred(:, 1) = 1.0
+
+      k = 1
+      do i = 2, trngl%n_dev
+        do j = trngl%n_dev + 1 - i + 1, trngl%n_dev
+          X_pred(k, i) = 1
+          if (j /= 1) X_pred(k, trngl%n_dev + j - 1) = 1
+          k = k + 1
+        end do
+      end do
+
+      y_pred = exp(matmul(X_pred, betas_boot))
+
+      do i_sim = 1, n_sim
+        k = 1
+        do i = 2, trngl%n_dev
+          do j = trngl%n_dev + 1 - i + 1, trngl%n_dev
+            triangle_pred(i, j) = y_pred(k)
+            k = k + 1
+          end do
+        end do
+
+        do i = 1, trngl%n_pred
+          lambda = y_pred(i)
+          if (lambda > 1e7) cycle main_loop
+          y_pred(i) = rpois(lambda)
+        end do
+
+        reserve(n_boot * (i_boot - 1) + i_sim) = sum(y_pred)
+      end do
+
+      i_boot = i_boot + 1
+      if (progress) call pgb_incr(pgb, n_sim)
+    end do main_loop
+  end function odp_param_boot
+
+  function odp_resid_boot(trngl, n_boot, n_sim, pgb, status) result(reserve)
+    type(t_odp_triangle), intent(inout) :: trngl
+    integer(c_int), intent(in) :: n_boot, n_sim
+    type(c_ptr), intent(in), value :: pgb
+    integer, intent(inout) :: status
+
+    real(c_double) :: reserve(n_boot * n_sim)
+    integer(c_int) :: n_pts
+    real(c_double), allocatable :: X_pred(:, :), y_pred(:)
+    real(c_double), allocatable :: triangle_boot(:, :), betas_boot(:), resids_boot(:, :)
+    real(c_double), allocatable :: triangle_pred(:, :)
+
+    real(c_double) :: resid_sim
+
+    real(c_double), allocatable :: flat_resids(:)
+
+    integer(c_int) :: i, j, k, i_boot, i_sim
+
+    logical :: progress
+    
+    allocate (X_pred(trngl%n_pred, trngl%n_cov))
+    allocate (y_pred(trngl%n_pred))
+    allocate (triangle_boot(trngl%n_dev, trngl%n_dev))
+    allocate (betas_boot(trngl%n_cov))
+    allocate (resids_boot(trngl%n_dev, trngl%n_dev))
+    allocate (triangle_pred(trngl%n_dev, trngl%n_dev))
+
+    progress = c_associated(pgb)
+
+    n_pts = count(trngl%mask)
+    allocate (flat_resids(n_pts), source=0._c_double)
+    call trngl%fit_glm(use_mask=.false., status=status)
+    flat_resids = pack(trngl%resids, trngl%mask)
+
+    i_boot = 1
+    main_loop: do while (i_boot <= n_boot)
+      triangle_boot = 0
+      resids_boot = 0
+      do i = 1, trngl%n_dev
+        do j = 1, trngl%n_dev + 1 - i
+          resids_boot(i, j) = flat_resids(1 + int(n_pts * runif()))
+          triangle_boot(i, j) = resids_boot(i, j) * sqrt(trngl%triangle_fit(i, j)) + trngl%triangle_fit(i, j)
+          if (triangle_boot(i, j) <= 0) cycle main_loop
+        end do
+      end do
+
+      call trngl%fit_glm_boot(triangle_boot, use_mask=.false., status=status)
+      if (status == FAILURE) cycle main_loop
+
+      X_pred = 0.0
+      X_pred(:, 1) = 1.0
+
+      k = 1
+      do i = 2, trngl%n_dev
+        do j = trngl%n_dev + 1 - i + 1, trngl%n_dev
+          X_pred(k, i) = 1
+          if (j /= 1) X_pred(k, trngl%n_dev + j - 1) = 1
+          k = k + 1
+        end do
+      end do
+
+      y_pred = exp(matmul(X_pred, trngl%betas_boot))
+      do i_sim = 1, n_sim
+        triangle_pred = 0
+        k = 1
+        do i = 2, trngl%n_dev
+          do j = trngl%n_dev + 1 - i + 1, trngl%n_dev
+            triangle_pred(i, j) = y_pred(k)
+            k = k + 1
+          end do
+        end do
+
+        k = 1
+        do i = 2, trngl%n_dev
+          do j = trngl%n_dev + 1 - i + 1, trngl%n_dev
+            resid_sim = flat_resids(1 + int(n_pts * runif()))
+            triangle_pred(i, j) = triangle_pred(i, j) + resid_sim * sqrt(triangle_pred(i, j))
+          end do
+        end do
+
+        reserve(n_boot * (i_boot - 1) + i_sim) = real(sum(triangle_pred), kind=c_double)
+      end do
+      
+      i_boot = i_boot + 1
+      if (progress) call pgb_incr(pgb, n_sim)
+    end do main_loop
+  end function odp_resid_boot
+
+  subroutine odp_param_boot_cpp(n_dev, triangle, n_boot, n_sim, dist, mask, reserve, pgb) bind(c, name="odp_param_boot")
+    integer(c_int), intent(in), value :: n_boot, n_sim, n_dev
+    logical(c_bool), intent(in) :: mask(n_dev, n_dev)
+    real(c_double), intent(in) :: triangle(n_dev, n_dev)
+    integer(c_int), intent(in), value :: dist
+    real(c_double), intent(out) :: reserve(n_boot * n_sim)
+    type(c_ptr), intent(in), value :: pgb
+
+    integer :: status
+    type(t_odp_triangle) :: trngl
+
+    call trngl%init(triangle, logical(mask))
+    reserve = odp_param_boot(trngl, n_boot, n_sim, dist, pgb, status)
+  end subroutine odp_param_boot_cpp
+
+  subroutine odp_resid_boot_cpp(n_dev, triangle, n_boot, n_sim, mask, reserve, pgb) bind(c, name="odp_resid_boot")
+    integer(c_int), intent(in), value :: n_boot, n_sim, n_dev
+    logical(c_bool), intent(in) :: mask(n_dev, n_dev)
+    real(c_double), intent(in) :: triangle(n_dev, n_dev)
+    real(c_double), intent(out) :: reserve(n_boot * n_sim)
+    type(c_ptr), intent(in), value :: pgb
+
+    integer :: status
+    type(t_odp_triangle) :: trngl
+
+    call trngl%init(triangle, logical(mask))
+    reserve = odp_resid_boot(trngl, n_boot, n_sim, pgb, status)
+  end subroutine odp_resid_boot_cpp
+
+
+end module mod_odp
---
+++ src/mod_odp_triangle.f90
@@ -0,0 +1,254 @@
+module mod_odp_triangle
+  use, intrinsic :: iso_c_binding
+  use, intrinsic :: ieee_arithmetic
+  use mod_config
+
+  implicit none
+
+  !> @brief Derived type representing an ODP triangle.
+  !! @param n_dev Number of rows/columns in the triangle.
+  !! @param n_cov Number of covariates in the GLM model.
+  !! @param n_fit Number of fitted values in the GLM model.
+  !! @param n_obs Number of observations used in the GLM model.
+  !! This excludes the masked elements.
+  type t_odp_triangle
+    integer(c_int) :: n_dev, n_cov, n_fit, n_obs, n_pred
+    real(c_double), allocatable :: triangle(:, :), triangle_fit(:, :)
+    real(c_double), allocatable :: resids(:, :)
+    real(c_double), allocatable :: betas(:), betas_boot(:)
+    real(c_double) :: disp
+    logical(c_bool), allocatable :: mask(:, :)
+    logical :: first_call = .true.
+
+  contains
+    procedure, public :: init => odp_init_triangle, fit_glm_helper, fit_glm_boot, fit_glm
+    final :: destructor
+  end type t_odp_triangle
+
+contains
+
+  subroutine odp_init_triangle(this, triangle, mask)
+    class(t_odp_triangle) :: this
+    real(c_double), intent(in) :: triangle(:, :)
+    logical, optional, intent(in) :: mask(:, :)
+
+    integer :: i, j
+
+    this%triangle = triangle
+    this%n_dev = size(triangle, 1)
+    this%n_cov = 2 * this%n_dev - 1
+    this%n_fit = (this%n_dev**2 + this%n_dev) / 2
+
+    if (this%first_call) then
+      allocate (this%triangle_fit(this%n_dev, this%n_dev))
+
+      if (present(mask)) then
+        this%mask = mask
+        this%n_obs = count(this%mask)
+        this%n_pred = this%n_dev**2 - this%n_obs
+      else
+      
+      this%n_pred = this%n_dev**2 - this%n_fit
+        allocate (this%mask(this%n_dev, this%n_dev), source=.true._c_bool)
+        do j = 1, this%n_dev
+          do i = this%n_dev + 2 - j, this%n_dev
+            this%mask(i, j) = .false.
+          end do
+        end do
+      end if
+
+      allocate (this%betas(this%n_cov), source=0._c_double)
+      allocate (this%resids(this%n_dev, this%n_dev), source=0._c_double)
+
+      this%first_call = .false.
+    end if
+  end subroutine odp_init_triangle
+
+  function fit_glm_helper(this, triangle, use_mask, status) result(betas)
+    class(t_odp_triangle) :: this
+    real(c_double), intent(in) :: triangle(:, :)
+    logical, intent(in) :: use_mask
+    integer(c_int), intent(out) :: status
+
+    real(c_double), allocatable :: X(:, :), y(:)
+    real(c_double), allocatable :: y_fit(:), X_fit(:, :), y_model(:)
+    real(c_double), allocatable :: rhs(:), lhs(:, :), mu(:)
+    real(c_double), allocatable :: W(:, :), z(:), eta(:), bbetas_old(:), betas(:)
+
+    integer(c_int) :: i, j, k, l
+    real(c_double) :: diff, eps
+    logical(c_bool), allocatable :: mask_loc(:, :)
+    integer(c_int) :: info
+    integer(c_int) :: lwork
+    real(c_double), allocatable :: work(:, :)
+    integer(c_int), parameter :: max_iter = 1e3
+    integer(c_int) :: n_iter
+
+    allocate (mask_loc(this%n_dev, this%n_dev), source=.true._c_bool)
+    if (use_mask) then
+      mask_loc = this%mask
+    else
+      do j = 1, this%n_dev
+        do i = this%n_dev + 2 - j, this%n_dev
+          mask_loc(i, j) = .false.
+        end do
+      end do
+    end if
+
+    ! Compute GLM matrix dimensions.
+    this%n_obs = this%n_fit
+    if (use_mask) this%n_obs = count(this%mask)
+
+    allocate (X(this%n_obs, this%n_cov))
+    allocate (y(this%n_obs))
+    allocate (y_model(this%n_obs))
+    allocate (W(this%n_obs, this%n_obs))
+    allocate (z(this%n_obs))
+    allocate (eta(this%n_obs))
+    allocate (mu(this%n_obs))
+    allocate (X_fit(this%n_fit, this%n_cov))
+    allocate (y_fit(this%n_fit))
+
+    ! Allocate LAPACK helper variables.
+    allocate (rhs(this%n_cov))
+    allocate (lhs(this%n_cov, this%n_cov))
+    allocate (betas(this%n_cov))
+    allocate (bbetas_old(this%n_cov))
+
+    lwork = max(1, this%n_cov * this%n_obs + max(this%n_cov * this%n_obs, 1))
+    allocate (work(lwork, lwork), source=0._c_double)
+
+    ! Set up feature matrix and response vector.
+    X = 0
+    X(:, 1) = 1._c_double
+
+    X_fit = 0
+    X_fit(:, 1) = 1._c_double
+
+    l = 1
+    k = 1
+    do i = 1, this%n_dev
+      do j = 1, this%n_dev + 1 - i
+        if (mask_loc(i, j)) then
+          if (i /= 1) X(k, i) = 1
+          if (j /= 1) X(k, this%n_dev + j - 1) = 1
+          y(k) = triangle(i, j)
+          k = k + 1
+        end if
+        if (i /= 1) X_fit(l, i) = 1
+        if (j /= 1) X_fit(l, this%n_dev + j - 1) = 1
+        l = l + 1
+      end do
+    end do
+
+    ! Initialise GLM coefficients.
+    mu = y + 0.1
+    eta = log(mu)
+
+    ! Fit Poisson GLM using IRWLS.
+    betas = 0
+    info = 0
+    diff = 1E6
+    eps = 1E-5
+    n_iter = 0
+    status = SUCCESS
+    do while (diff > eps .and. n_iter < max_iter)
+      bbetas_old = betas
+
+      W = 0
+      do i = 1, this%n_obs
+        W(i, i) = sqrt(mu(i))
+      end do
+
+      z = eta + y / mu - 1
+      lhs = matmul(W, X)
+      rhs = matmul(W, z)
+
+      if (any(isnan(rhs)) .or. any(isnan(lhs))) then
+        status = FAILURE
+        return
+      end if
+
+      call dgels('N', this%n_obs, this%n_cov, 1, lhs, this%n_obs, rhs, this%n_obs, work, lwork, info)
+
+      betas = rhs(1:this%n_cov)
+      diff = norm2(betas - bbetas_old)
+
+      eta = matmul(X, betas)
+      mu = exp(eta)
+
+      if (any(mu <= 0)) then
+        status = FAILURE
+        return
+      end if
+      if (any(isnan(mu)) .or. .not. all(ieee_is_finite(mu))) then
+        status = FAILURE
+        return
+      end if
+
+      n_iter = n_iter + 1
+    end do
+
+    if (n_iter == max_iter) then
+      status = FAILURE
+      return
+    end if
+
+    y_fit = exp(matmul(X_fit, betas))
+    y_model = exp(matmul(X, betas))
+
+    this%disp = sum((y - y_model)**2 / y_model) / (this%n_obs - this%n_cov)
+
+    k = 1
+    do i = 1, this%n_dev
+      do j = 1, this%n_dev + 1 - i
+        this%triangle_fit(i, j) = y_fit(k)
+        k = k + 1
+      end do
+    end do
+
+    do i = 1, this%n_dev
+      do j = 1, this%n_dev + 1 - i
+        this%resids(i, j) = (triangle(i, j) - this%triangle_fit(i, j)) / sqrt(this%triangle_fit(i, j))
+      end do
+    end do
+
+    deallocate (y)
+    deallocate (y_model)
+    deallocate (W)
+    deallocate (z)
+    deallocate (eta)
+    deallocate (mu)
+    deallocate (X_fit)
+    deallocate (y_fit)
+  end function fit_glm_helper
+
+  subroutine fit_glm(this, use_mask, status)
+    class(t_odp_triangle), intent(inout) :: this
+    logical, intent(in) :: use_mask
+    integer, intent(out) :: status
+
+    this%betas = this%fit_glm_helper(this%triangle, use_mask, status)
+  end subroutine fit_glm
+
+  subroutine fit_glm_boot(this, triangle_boot, use_mask, status)
+    class(t_odp_triangle), intent(inout) :: this
+    real(c_double), intent(in) :: triangle_boot(:, :)
+    logical, intent(in) :: use_mask
+    integer, intent(out) :: status
+
+    this%betas_boot = this%fit_glm_helper(triangle_boot, use_mask, status)
+  end subroutine fit_glm_boot
+
+  subroutine destructor(this)
+    type(t_odp_triangle) :: this
+
+    if (allocated(this%triangle)) deallocate (this%triangle)
+    if (allocated(this%triangle_fit)) deallocate (this%triangle_fit)
+    if (allocated(this%resids)) deallocate (this%resids)
+    if (allocated(this%betas)) deallocate (this%betas)
+    if (allocated(this%betas_boot)) deallocate (this%betas_boot)
+    if (allocated(this%mask)) deallocate (this%mask)
+  end subroutine destructor
+
+end module mod_odp_triangle
